97ba79a4c37060acdab3249d75d87437a7a2b36e
mt25acl <mt25acl@herts.ac.uk>
2026-04-23 00:57:51 +0100

    add Booking.java file in flc.model

---
+++ src/flc/model/Booking.java
@@ -0,0 +1,45 @@
+package flc.model;
+
+/**
+ * Represents a booking made by a member for a specific lesson.
+ * Acts as a link between Member and Lesson.
+ */
+public class Booking {
+
+    private final String bookingId;
+    private final Member member;
+    private Lesson lesson;
+    private final long bookingTimestamp;
+
+    public Booking(String bookingId, Member member, Lesson lesson) {
+        this.bookingId = bookingId;
+        this.member = member;
+        this.lesson = lesson;
+        this.bookingTimestamp = System.currentTimeMillis();
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    public long getBookingTimestamp() {
+        return bookingTimestamp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Booking[%s] %s -> %s", bookingId, member.getName(), lesson);
+    }
+}
--- src/furzefield/leisure/centre/system/FurzefieldLeisureCentreSystem.java
+++ src/furzefield/leisure/centre/system/FurzefieldLeisureCentreSystem.java
@@ -14,6 +14,8 @@ public class FurzefieldLeisureCentreSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
         // TODO code application logic here
     }
     
